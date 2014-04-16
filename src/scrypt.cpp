98b290a3e07cd85bb77244f53a38913198426611
xinternetx <sobrelaweb@gmail.com>
2014-04-16 18:19:34 +0200

    Update scrypt.cpp

--- src/scrypt.cpp
+++ src/scrypt.cpp
@@ -276,7 +276,7 @@ void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scrat
         {
 	N = 2048;
 	}
-	fprintf(stdout, "Factor ENE scrypt.cpp fprintf es %i", N);
+	//fprintf(stdout, "Factor ENE scrypt.cpp fprintf es %i", N);
 	for (i = 0; i < N; i++) {
 		memcpy(&V[i * 32], X, 128);
 		xor_salsa8(&X[0], &X[16]);
